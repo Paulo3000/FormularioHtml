2d5f33ba11cc5658bbe4f15514c32212c86af454
Paulo3000 <1988paulosergio@gmail.com>
2022-01-11 17:21:14 -0300

    criado formulario

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+ <head>
+  <title>Somar valores</title>
+ </head>
+<body>
+
+<form name ="f_cad" method="get" action="index.php">
+
+<label>Nome:</label><br/>
+<input type="text" name="f_nome" size="40" maxlength="30"/><br/><br/>
+
+
+<input type="submit" value="Enviar"/>
+<input type="reset" value="Limpar"/>
+</form>
+
+<?php
+
+
+
+ 
+
+?>
+
+</body>
+</html>
